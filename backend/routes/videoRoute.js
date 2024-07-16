bce991f3c355be9f70ff8262a93dcde5f878c86c
agyinjohn <agyinjohn100@gmail.com>
2024-07-16 18:50:09 +0000

    Jwt error

--- backend/routes/videoRoute.js
+++ backend/routes/videoRoute.js
@@ -9,7 +9,7 @@ const { protect, admin } = require("../utils/middlewares/authmiddleware");
 const Videorouter = express.Router();
 
 Videorouter.post("/upload", protect, admin, uploadVideo);
-Videorouter.get("/stream/:id", protect, streamVideo);
+Videorouter.get("/stream/:id", streamVideo);
 Videorouter.get("/listVideos", protect, listVideos);
 
 module.exports = Videorouter;
