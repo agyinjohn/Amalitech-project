139eba3c9265eb0a23fca9939d3c7c760c29c396
agyinjohn <agyinjohn100@gmail.com>
2024-07-17 20:30:44 +0000

    Testing for review

--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,26 @@ const app = express();
 // const jwtSecret = crypto.randomBytes(64).toString("hex");
 // console.log(jwtSecret);
 //Middlewares for security purposes
-const corsOptions = {
-  origin: "https://6690b9a8e471f9c7493fb8c4--cute-cuchufli-b9e947.netlify.app", // Allow both ports
-  methods: ["GET", "POST"], // Allowed methods
-  allowedHeaders: ["Content-Type"], // Allowed headers
-  credentials: true, // Allow cookies to be sent
-};
+const allowedOrigins = [
+  "https://6690b9a8e471f9c7493fb8c4--cute-cuchufli-b9e947.netlify.app",
+  "https://66969e37c89110de29a269ea--cute-cuchufli-b9e947.netlify.app",
+  "http://localhost:5173",
+  // Add any other allowed origins here
+];
 
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // if your request includes credentials
+  })
+);
+
 app.use(express.json());
 app.use(helmet());
 app.use("/api", router);
